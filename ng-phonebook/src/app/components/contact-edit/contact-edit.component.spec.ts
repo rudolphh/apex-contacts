1e029a14a313af0902a68d3cc8146c559f772e1b
Rudy A. Hernandez <rudolpharthur@gmail.com>
2022-02-10 23:58:26 -0800

    update

--- ng-phonebook/src/app/components/contact-edit/contact-edit.component.spec.ts
+++ ng-phonebook/src/app/components/contact-edit/contact-edit.component.spec.ts
@@ -1,13 +1,29 @@
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 
+import { Contact } from '../contact-list/contact';
+import { ContactDataService } from 'src/app/services/contact-data.service';
 import { ContactEditComponent } from './contact-edit.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
 
 describe('ContactEditComponent', () => {
   let component: ContactEditComponent;
   let fixture: ComponentFixture<ContactEditComponent>;
+  const contactDataServiceSpy = jasmine.createSpyObj<ContactDataService>(
+    'ContactDataService',
+    {
+      updateContact: of({})
+    },
+    { editContact$: of() }
+  );
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule, ReactiveFormsModule],
+      providers: [
+        { provide: ContactDataService, useValue: contactDataServiceSpy}
+      ],
       declarations: [ ContactEditComponent ]
     })
     .compileComponents();
@@ -16,6 +32,7 @@ describe('ContactEditComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ContactEditComponent);
     component = fixture.componentInstance;
+    component.contact = { id: 0, firstName: 'John', lastName: 'Doe', email: 'john@doe.com'} as Contact;
     fixture.detectChanges();
   });
 
